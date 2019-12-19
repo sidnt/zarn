b4bb0fecb9e295ae61734726cc7175316b69b97c
sidnt <sidnt@users.noreply.github.com>
2019-12-19 23:54:08 +0530

    demo props and nesting components

---
+++ src/main/scala/hello/world/HW1.scala
@@ -0,0 +1,76 @@
+package hello.world
+
+import slinky.core.annotations.react // @react annotation
+import slinky.core._ // `Component`
+import slinky.native._  // `View` & `Text` from react-native
+import scala.scalajs.js.Dynamic.literal
+import slinky.core.facade.ReactElement
+
+
+/* equivalent react-native code
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class Greeting extends Component {
+  render() {
+    return (
+      <View style={{alignItems: 'center'}}>
+        <Text>Hello {this.props.name}!</Text>
+      </View>
+    );
+  }
+}
+*/
+@react class Greeting extends Component {
+  
+  type Props = String
+  type State = Unit
+  def initialState: State = ()
+  
+  def render(): ReactElement =
+    View(
+      style = literal(
+        alignItems = "center"
+      )
+    )(
+      Text(s"Hello ${props}!")
+    )
+
+}
+
+/* react-native code
+export default class LotsOfGreetings extends Component {
+  render() {
+    return (
+      <View style={{alignItems: 'center', top: 50}}>
+        <Greeting name='Rexxar' />
+        <Greeting name='Jaina' />
+        <Greeting name='Valeera' />
+      </View>
+    );
+  }
+}
+*/
+@react class SomeGreetings extends Component {
+
+  type Props = Unit
+  type State = Unit
+  def initialState: State = ()
+
+  def render(): ReactElement =
+    View(
+      style = literal(
+        alignItems = "center",
+        top = 50
+      )
+    )(
+      Greeting("Rexxar"),
+      Greeting("Jaina"),
+      Greeting("Valeera")
+    )
+
+}
+
+object HW1 {
+  def apply() = SomeGreetings
+}
