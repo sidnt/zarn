58afe62d51bc522f218be37f17947c9e8afcff10
sidnt <sidnt@users.noreply.github.com>
2019-12-31 11:49:19 +0530

    abstract out centerStyle

---
+++ src/main/scala/hello/world/package.scala
@@ -0,0 +1,13 @@
+package hello
+
+import scala.scalajs.js.Dynamic.literal
+
+package object world {
+
+  val centerStyle = literal(
+    flex = 1,
+    justifyContent = "center",
+    alignItems = "center"
+  )
+
+}
