46be42a4d3d8d323e7479cdd940b7ab5431fc57d
sidnt <sidnt@users.noreply.github.com>
2020-01-26 11:22:24 +0530

    different manner of loading a functional component

--- src/main/scala/hello/zio/eg1.scala
+++ src/main/scala/hello/zio/eg1.scala
@@ -11,13 +11,14 @@ import slinky.native._
     View(style = centerStyle)(Text("ℍ\uD835\uDD56\uD835\uDD5D\uD835\uDD5D\uD835\uDD60. \uD835\uDD3D\uD835\uDD63\uD835\uDD60\uD835\uDD5E \uD835\uDD52 \uD835\uDD3D\uD835\uDD66\uD835\uDD5F\uD835\uDD54\uD835\uDD65\uD835\uDD5A\uD835\uDD60\uD835\uDD5F\uD835\uDD52\uD835\uDD5D ℂ\uD835\uDD60\uD835\uDD5E\uD835\uDD61\uD835\uDD60\uD835\uDD5F\uD835\uDD56\uD835\uDD5F\uD835\uDD65."))
   }
 }
-
-@react class FunCompLoader extends StatelessComponent {
-  type Props = Unit
-  def render(): ReactElement =
-    FunComp.component.apply(()).withKey("somekey")
-}
+//
+//@react class FunCompLoader extends StatelessComponent {
+//  type Props = Unit
+//  def render(): ReactElement =
+//    FunComp.component.apply(()).withKey("somekey")
+//}
 
 object eg1 {
-  def apply() = FunCompLoader.componentConstructor
+//  def apply() = FunCompLoader.componentConstructor
+  def apply(): ReactComponentClass[_] = FunComp.component
 }
