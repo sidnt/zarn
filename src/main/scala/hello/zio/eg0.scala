19d39d792fdd4505f9d5d87dc42722ec8b095ae9
sidnt <sidnt@users.noreply.github.com>
2020-01-30 23:50:05 +0530

    conciser FunComp example

---
+++ src/main/scala/hello/zio/eg0.scala
@@ -0,0 +1,16 @@
+package hello.zio
+
+import hello.world.centerStyle
+import slinky.core._
+import slinky.core.annotations.react
+import slinky.native._
+
+@react object FunComp {
+  val component = FunctionalComponent[Unit] { _ =>
+    View(style = centerStyle)(Text("ℍ\uD835\uDD56\uD835\uDD5D\uD835\uDD5D\uD835\uDD60. \uD835\uDD3D\uD835\uDD63\uD835\uDD60\uD835\uDD5E \uD835\uDD52 \uD835\uDD3D\uD835\uDD66\uD835\uDD5F\uD835\uDD54\uD835\uDD65\uD835\uDD5A\uD835\uDD60\uD835\uDD5F\uD835\uDD52\uD835\uDD5D ℂ\uD835\uDD60\uD835\uDD5E\uD835\uDD61\uD835\uDD60\uD835\uDD5F\uD835\uDD56\uD835\uDD5F\uD835\uDD65."))
+  }
+}
+
+object eg0 {
+  def apply(): ReactComponentClass[_] = FunComp.component
+}
